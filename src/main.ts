e97cd69204f794a8a96210a30c54bb48324d49ec
n0ir <smile955@inbox.ru>
2018-10-25 02:26:23 +0300

    update

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { WsAdapter } from '@nestjs/websockets/adapters';
+import { ApplicationModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(ApplicationModule);
+  app.useWebSocketAdapter(new WsAdapter(app.getHttpServer()));
+  await app.listen(8080, () => {
+    console.log('Start');
+  });
+}
+bootstrap();
