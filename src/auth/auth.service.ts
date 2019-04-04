3d3fc92099086a8f3e433d0833c788bd04f8b7e7
n0ir <smile955@inbox.ru>
2019-04-05 00:44:49 +0300

    auth update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,17 @@
-import { pbkdf2Sync, randomBytes } from 'crypto';
 import { Injectable } from '@nestjs/common';
+import * as upash from 'upash';
+import * as pbkdf2 from '@phc/pbkdf2';
 import { UserService } from '../user/user.service';
 import { TokensService } from './tokens.service';
+import { User } from '../user/models/user';
+import { Tokens } from './models/tokens';
+
+interface IAuthData {
+  username: string;
+  password: string;
+}
+
+upash.install('pbkdf2', pbkdf2);
 
 @Injectable()
 export class AuthService {
@@ -10,51 +20,41 @@ export class AuthService {
     private readonly tokensService: TokensService,
   ) {}
 
-  private getHash(password, salt) {
-    return pbkdf2Sync(password, salt, 2048, 32, 'sha512').toString('hex');
+  async hashPassword(password: string): Promise<string> {
+    return await upash.hash(password);
   }
 
-  public hashPassword(password) {
-    const salt = randomBytes(32).toString('hex');
-    const hash = this.getHash(password, salt);
-    return [salt, hash].join('$');
+  async checkPassword(hashstr: string, password: string): Promise<boolean> {
+    return await upash.verify(hashstr, password);
   }
 
-  public checkPassword(saltedPasswordHash, candidatePassword) {
-    const originalHash = saltedPasswordHash.split('$')[1];
-    const salt = saltedPasswordHash.split('$')[0];
-    const hash = this.getHash(candidatePassword, salt);
-    return hash == originalHash;
-  }
-
-  async signUp({ username, password }) {
+  async signUp({ username, password }: IAuthData): Promise<Tokens> {
     const checkUser = await this.userService.findOne({
       where: {
         username,
       },
     });
+
     if (checkUser) {
       throw new Error('Duplicate user');
     }
+
     const newUser = await this.userService.create({
       username,
-      password: this.hashPassword(password),
+      password: await this.hashPassword(password),
     });
 
     return await this.tokensService.generateTokens(newUser.userId);
   }
 
-  async logIn(username: string, password: string) {
-    const user = await this.userService.findOne({
-      where: {
-        username,
-      },
-    });
+  async logIn({ username, password }: IAuthData): Promise<User> {
+    const user = await this.userService.findByName(username);
 
     if (!user) {
       throw new Error('User not found');
     }
-    const isValidPassword = this.checkPassword(user.password, password);
+
+    const isValidPassword = await this.checkPassword(user.password, password);
 
     if (!isValidPassword) {
       throw new Error('Invalid password');
--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
-import { TokensService } from '../tokens.service';
+import { ExtractJwt, Strategy, VerifiedCallback } from 'passport-jwt';
+import { TokensService, JwtPayload } from '../tokens.service';
 
 import { config } from '../../../config';
 
@@ -17,12 +17,13 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload, done) {
-    try {
-      const user = await this.tokensService.verify(payload);
-      done(null, user);
-    } catch (error) {
-      done(error, false);
+  async validate(payload: JwtPayload, done: VerifiedCallback) {
+    const user = await this.tokensService.verify(payload);
+
+    if (!user) {
+      return done(new UnauthorizedException(), false);
     }
+
+    return done(null, user, payload.iat);
   }
 }
